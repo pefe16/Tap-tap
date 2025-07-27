f7934636116754e729aacc083ca1be2ad579e0b6
pefe16 <efepekdemir11@gmail.com>
2025-07-27 15:52:30 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
-self.addEventListener('install', e => { e.waitUntil(caches.open('tap-tap-cache').then(cache => { return cache.addAll(['./', './index.html', './main.js', './manifest.json', './icon.png']); })); }); self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(response => response || fetch(e.request))); });
+const cacheName = 'tap-tap-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
