3815399a1ffea187ef2083604c7818c657c7530d
pefe16 <efepekdemir11@gmail.com>
2025-07-27 17:05:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,46 @@
 const CACHE_NAME = "asteroid-defense-cache-v1";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./main.js",
-  "./manifest.json",
-  "./favicon.ico",
-  "./icon.png"
+  "index.html",
+  "main.js",
+  "manifest.json",
+  "icon.png",
+  "favicon.ico"
 ];
 
-self.addEventListener("install", event => {
+// Servis worker kurulunca gerekli dosyaları önbelleğe al
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log("[Service Worker] Caching app files");
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
+// İsteklerde önce önbellekten sonra internetten çek
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Yeni sürüm geldiğinde eski cache’i temizle
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames.map((name) => {
+            if (name !== CACHE_NAME) {
+              console.log("[Service Worker] Deleting old cache:", name);
+              return caches.delete(name);
+            }
+          })
+        )
+      )
   );
 });
