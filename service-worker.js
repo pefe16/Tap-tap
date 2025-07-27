f20ba4c8440036ee3992f78e278e0dbbc845a437
pefe16 <efepekdemir11@gmail.com>
2025-07-27 16:44:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,37 @@
-const cacheName = 'tap-tap-cache-v1';
-const filesToCache = [
-  './',
-  './index.html',
-  './main.js',
-  './manifest.json',
-  './icon.png',
-  './favicon.ico',
-  './space-bg.jpg'
+const CACHE_NAME = "asteroid-defense-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./main.js",
+  "./manifest.json",
+  "./favicon.ico",
+  "./icon.png"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys.map(k => {
-          if (k !== cacheName) return caches.delete(k);
+          if (k !== CACHE_NAME) return caches.delete(k);
         })
-      )
-    )
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
